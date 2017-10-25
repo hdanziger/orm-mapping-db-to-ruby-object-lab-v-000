9ea2097a535dd6d86e0faac91b529b032ddbfbe9
Hannah Danziger <hdanziger8@gmail.com>
2017-10-25 18:52:13 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -101,8 +101,9 @@ class Student
 
   def self.first_student_in_grade_10
     sql = <<-SQL
-    SELECT grade
+    SELECT students.id
     FROM students
+    ORDER BY (grade)
     WHERE grade = '10'
     LIMIT 1
     SQL
