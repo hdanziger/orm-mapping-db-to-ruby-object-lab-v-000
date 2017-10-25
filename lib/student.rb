4517346f3aa6ab9a75889d9d6c65f8c744fc70f0
Hannah Danziger <hdanziger8@gmail.com>
2017-10-25 18:51:09 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -98,4 +98,14 @@ class Student
       self.new_from_db(row)
     end
   end
+
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+    SELECT grade 
+    FROM students 
+    WHERE grade = '10'
+    LIMIT 1
+    SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
 end
